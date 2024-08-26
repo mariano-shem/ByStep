1eec3117b6ed2504ac52ad51650db69a33dadb62
sheeemboi <shemmariano56@gmail.com>
2024-08-26 09:53:40 +0800

    sidebar improvement

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 let w = document.documentElement.clientWidth || window.innerWidth;
-  document.querySelector("#sidebar-open").addEventListener("click",
+let q = document.querySelectorAll(".viewcard");
+
+document.querySelector(".sidebar-btn").addEventListener("click",
     function() {
-      document.querySelector(".mobile-sidebar").style.width = "60%";
+      document.querySelector(".mobile-sidebar").style.width = "50%";
       document.querySelector(".main-page").style.opacity = "0.4";
       document.querySelector(".header").style.opacity = "0.4";
+      for (let i = 0; i < q.length; i++) {
+        q[i].classList.add("disable-link");
+      };
   });
   
-  document.querySelector("#sidebar-close").addEventListener("click",
+document.querySelector(".closebtn").addEventListener("click",
     function() {
       document.querySelector(".mobile-sidebar").style.width = "0";
       document.querySelector(".main-page").style.opacity = "1";
       document.querySelector(".header").style.opacity = "1";
+      for (let i = 0; i < q.length; i++) {
+        q[i].classList.remove("disable-link");
+      };
+
   });
-  
-  document.querySelector(".main-page").addEventListener("click",
+
+document.querySelector(".main-page").addEventListener("click",
     function() {
       document.querySelector(".mobile-sidebar").style.width = "0";
       document.querySelector(".main-page").style.opacity = "1";
       document.querySelector(".header").style.opacity = "1";
+      for (let i = 0; i < q.length; i++) {
+        q[i].classList.remove("disable-link");
+      };
   });
 
 
