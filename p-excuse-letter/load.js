5c54c4876a843aacd5b7065ea859fcfc37c5631c
sheeemboi <shemmariano56@gmail.com>
2024-08-26 13:29:13 +0800

    content load test fix

--- p-excuse-letter/load.js
+++ p-excuse-letter/load.js
@@ -19,6 +19,7 @@ async function loadAccordionContents() {
       document.getElementById("content-3").innerHTML = doc.querySelector("#content-3").innerHTML;
       document.getElementById("content-4").innerHTML = doc.querySelector("#content-4").innerHTML;
       
+
       
     } else {
       console.error("No content.", response.status, response.statusText);
@@ -26,53 +27,45 @@ async function loadAccordionContents() {
 
   } catch (error) {
     console.error("Please refresh page.",error);
+  } finally {
+    function openAccordion(){
+      const stepOne = document.querySelector(".step-one");
+      const firstAccordion = stepOne.querySelector(".first-accordion-panel");
+      const stepBorder = stepOne.querySelector(".accordion-header");
+    
+      firstAccordion.style.marginTop = "0.1rem";
+      firstAccordion.style.marginBottom = "0.1rem";
+      firstAccordion.style.maxHeight = firstAccordion.scrollHeight + "px";
+    };
+    
+    function openCollapsible() {
+    
+      const collapseHead = document.querySelectorAll(".collapsible-header");
+    
+      collapseHead.forEach(
+        function(button) {
+          button.addEventListener("click", function(){
+            this.classList.toggle("open")
+    
+            const collapseContainer = this.closest(".accordion-panel");
+            const collapsePanel = this.nextElementSibling;
+            if (collapsePanel.style.maxHeight) {
+              collapsePanel.style.maxHeight = null;
+            } else {
+              collapsePanel.style.maxHeight = collapsePanel.scrollHeight + "px";
+              collapseContainer.style.marginTop = "0.1rem";
+              collapseContainer.style.marginBottom = "0.1rem";
+              collapseContainer.style.maxHeight = collapseContainer.scrollHeight + collapsePanel.scrollHeight + "px";
+            }
+          })
+        }
+      )
+    };
+
+    setTimeout(() => {
+      openAccordion()
+      openCollapsible()
+      }, 500)
   }
 }
 loadAccordionContents();
-//automatically opens the first collapsible
-document.addEventListener("DOMContentLoaded", openAccordion);
-
-function openAccordion(){
-  const stepOne = document.querySelector(".step-one");
-  const firstAccordion = stepOne.querySelector(".first-accordion-panel");
-  const stepBorder = stepOne.querySelector(".accordion-header");
-
-  firstAccordion.style.marginTop = "0.1rem";
-  firstAccordion.style.marginBottom = "0.1rem";
-  firstAccordion.style.maxHeight = firstAccordion.scrollHeight + "px";
-};
-
-function openCollapsible() {
-
-  const collapseHead = document.querySelectorAll(".collapsible-header");
-
-  collapseHead.forEach(
-    function(button) {
-      button.addEventListener("click", function(){
-        this.classList.toggle("open")
-
-        const collapseContainer = this.closest(".accordion-panel");
-        const collapsePanel = this.nextElementSibling;
-        if (collapsePanel.style.maxHeight) {
-          collapsePanel.style.maxHeight = null;
-        } else {
-          collapsePanel.style.maxHeight = collapsePanel.scrollHeight + "px";
-          collapseContainer.style.marginTop = "0.1rem";
-          collapseContainer.style.marginBottom = "0.1rem";
-          collapseContainer.style.maxHeight = collapseContainer.scrollHeight + collapsePanel.scrollHeight + "px";
-        }
-      })
-    }
-  )
-};
-
-
-//add delay in opening the collapsible to compensate for content loading
-//100 means 1 second delay
-function onPageLoad() {
-  setTimeout(() => {
-  openAccordion()
-  openCollapsible()
-  }, 500)
-}
-window.onload = onPageLoad;
